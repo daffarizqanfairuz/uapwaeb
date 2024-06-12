d0def8f2cde181a65acc516cbef938e9b146dc2c
atnatr <atrizki.anta@gmail.com>
2024-06-12 22:42:40 +0700

    SQL update

--- sql/uapwaeb.sql
+++ sql/uapwaeb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2024 at 02:41 AM
+-- Generation Time: Jun 12, 2024 at 05:42 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,6 +42,75 @@ INSERT INTO `akun` (`id`, `email`, `passwords`) VALUES
 (2, 'anta@gmail.com', 'anta'),
 (3, 'antaa@gmail.com', 'antaa');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+CREATE TABLE `game` (
+  `gameID` int(11) NOT NULL,
+  `genreID` int(11) DEFAULT NULL,
+  `platformID` int(11) DEFAULT NULL,
+  `nama` varchar(255) NOT NULL,
+  `deskripsi` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `game`
+--
+
+INSERT INTO `game` (`gameID`, `genreID`, `platformID`, `nama`, `deskripsi`) VALUES
+(1, 12, 1, 'Valorant', 'Game Shooter'),
+(2, 14, 2, 'Zero To Hero', 'AFK RPG gaem pH'),
+(3, 10, 3, 'Terraria', 'Game Sandbox'),
+(4, 1, 1, 'Devil May Gyat', 'Wla Wla Wla Plap'),
+(5, 1, 1, 'Edging Empire', 'You play as Sigma Empire where you try to rizz the minors to win the game!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `genreID` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `genre`
+--
+
+INSERT INTO `genre` (`genreID`, `nama`) VALUES
+(1, 'Action'),
+(10, 'Adventure'),
+(11, 'Fantasy'),
+(12, 'FPS'),
+(13, 'MMORPG'),
+(14, 'RPG'),
+(15, 'Arcade');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `platform`
+--
+
+CREATE TABLE `platform` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `platform`
+--
+
+INSERT INTO `platform` (`id`, `nama`) VALUES
+(1, 'PC'),
+(2, 'Mobile'),
+(3, 'Console');
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +122,26 @@ ALTER TABLE `akun`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`gameID`),
+  ADD KEY `genreID` (`genreID`),
+  ADD KEY `platformID` (`platformID`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genreID`);
+
+--
+-- Indexes for table `platform`
+--
+ALTER TABLE `platform`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +150,35 @@ ALTER TABLE `akun`
 --
 ALTER TABLE `akun`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `game`
+--
+ALTER TABLE `game`
+  MODIFY `gameID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genreID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `platform`
+--
+ALTER TABLE `platform`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`genreID`) REFERENCES `genre` (`genreID`),
+  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`platformID`) REFERENCES `platform` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
